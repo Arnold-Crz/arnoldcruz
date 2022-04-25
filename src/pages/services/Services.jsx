e92df20b57652ac850dda45629b2c25273120502
Arnold-Cruz <enriquecruzlife@gmail.com>
2022-04-25 01:04:39 -0600

    add new card

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -4,9 +4,11 @@ import './services.sass';
 
 import ImgServicesOne from '../../../assets/servicesone.svg';
 import IconArrow from '../../../assets/servicesarrow.svg';
+import ImgServicesTwo from '../../../assets/servicesdesing.svg';
 
 const services = [
   {
+    id: 1,
     title: 'PROGRAMADOR WEB',
     img: ImgServicesOne,
     descriptionFront:
@@ -14,11 +16,22 @@ const services = [
     descriptionBack:
       'Brindo soluciones tecnologicas, cumpliendo con cada uno de los requerimientos y objetivos que necesites al contratar mis servicios  como programador (desarrollo de un portafolio, pagina web, e-commerce, apliacion web.)',
   },
+  {
+    id: 2,
+    title: 'DISEÑADOR WEB',
+    img: ImgServicesTwo,
+    descriptionFront:
+      'Necesitas ayuda para un diseño web moderno profesional y así ofrecer tus productos o servicios a tus clientes, brindándoles una mejor navegación, integración de los mismos.',
+    descriptionBack:
+      'Necesitas ayuda para un diseño web moderno profesional y así ofrecer tus productos o servicios a tus clientes, brindándoles una mejor navegación, integración de los mismos.',
+  },
 ];
 
 function Services() {
   const [isOpen, setIsOpen] = useState(false);
-  const handleOpen = () => setIsOpen(!isOpen);
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className="ServiciosContent">
@@ -27,8 +40,8 @@ function Services() {
       <section>
         <article>
           {services.map((ser) => (
-            <div className="ServicesData">
-              <h2>{ser.title}</h2>
+            <div className="ServicesData" key={ser.id}>
+              <h2 className="color">{ser.title}</h2>
               <img src={ser.img} alt="servicesOne" />
               <p>{ser.descriptionFront}</p>
               <button onClick={handleOpen}>LEER MAS</button>
