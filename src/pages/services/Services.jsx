9b5a83aa8d576fb43e078ab1dc0bbb1d3f630f0d
Arnold-Cruz <enriquecruzlife@gmail.com>
2022-04-26 01:24:33 -0600

    mejoras de interfas

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -2,9 +2,34 @@ import React from 'react';
 
 import './services.sass';
 
-import ImgServicesOne from '../../../assets/servicesone.svg';
+import ImgServicesOne from '../../../assets/imgone.svg';
+import ImgServicesTwo from '../../../assets/imgtwo.svg';
+import ImgServicesThree from '../../../assets/imgthree.svg';
 import IconArrow from '../../../assets/servicesarrow.svg';
-import ImgServicesTwo from '../../../assets/servicesdesing.svg';
+
+const services = [
+  {
+    id: 1,
+    title: 'PROGRAMADOR WEB',
+    img: ImgServicesOne,
+    descriptionBack:
+      'Brindo soluciones tecnologicas, cumpliendo con cada uno de los requerimientos y objetivos que necesites al contratar mis servicios  como programador (desarrollo de un portafolio, pagina web, e-commerce, apliacion web.)',
+  },
+  {
+    id: 2,
+    title: 'DISEÑADOR WEB',
+    img: ImgServicesTwo,
+    descriptionBack:
+      'Necesitas ayuda para un diseño web moderno profesional y así ofrecer tus productos o servicios a tus clientes, brindándoles una mejor expericia de visualización.',
+  },
+  {
+    id: 3,
+    title: 'TECNICO INFORMATICO',
+    img: ImgServicesThree,
+    descriptionBack:
+      'Necesitas ayuda profesional para administrar o dar mantenimiento a tus equipos informaticos o darles una mejor rendimiento y funcionamiento.',
+  },
+];
 
 function Services() {
   const handleOpen = (e) => {
@@ -23,59 +48,27 @@ function Services() {
 
   return (
     <div className="ServiciosContent">
-      <h1>SERVICIOS</h1>
-      <span>SERVICIOS</span>
+      <h1>
+        SERVICIOS
+        <span>SERVICIOS</span>
+      </h1>
       <section>
         <article>
-          <div className="ServicesData">
-            <h2 className="color">PROGRAMADOR WEB</h2>
-            <img src={ImgServicesOne} alt="servicesOne" />
-            <p>
-              Brindo soluciones tecnologicas, cumpliendo con cada uno de los
-              requerimientos y objetivos que necesites al contratar mis
-              servicios como programador (desarrollo de un portafolio, pagina
-              web, e-commerce, apliacion web.)
-            </p>
-            <button onClick={(e) => handleOpen(e)}>LEER MAS</button>
-            <div className="MasInfo">
-              <h3>
-                Brindo soluciones tecnologicas, cumpliendo con cada uno de los
-                requerimientos y objetivos que necesites al contratar mis
-                servicios como programador (desarrollo de un portafolio, pagina
-                web, e-commerce, apliacion web.)
-              </h3>
-              <img
-                src={IconArrow}
-                alt="arrow"
-                onClick={(e) => handleClose(e)}
-              />
+          {services.map(({ id, title, img, descriptionBack }) => (
+            <div className="ServicesData" key={id}>
+              <h2 className="color">{title}</h2>
+              <img src={img} alt="servicesOne" />
+              <button onClick={(e) => handleOpen(e)}>Ver mas</button>
+              <div className="MasInfo">
+                <h3>{descriptionBack}</h3>
+                <img
+                  src={IconArrow}
+                  alt="arrow"
+                  onClick={(e) => handleClose(e)}
+                />
+              </div>
             </div>
-          </div>
-
-          <div className="ServicesData">
-            <h2 className="color">PROGRAMADOR WEB</h2>
-            <img src={ImgServicesTwo} alt="servicesOne" />
-            <p>
-              Brindo soluciones tecnologicas, cumpliendo con cada uno de los
-              requerimientos y objetivos que necesites al contratar mis
-              servicios como programador (desarrollo de un portafolio, pagina
-              web, e-commerce, apliacion web.)
-            </p>
-            <button onClick={(e) => handleOpen(e)}>LEER MAS</button>
-            <div className="MasInfo">
-              <h3>
-                Brindo soluciones tecnologicas, cumpliendo con cada uno de los
-                requerimientos y objetivos que necesites al contratar mis
-                servicios como programador (desarrollo de un portafolio, pagina
-                web, e-commerce, apliacion web.)
-              </h3>
-              <img
-                src={IconArrow}
-                alt="arrow"
-                onClick={(e) => handleClose(e)}
-              />
-            </div>
-          </div>
+          ))}
         </article>
       </section>
     </div>
